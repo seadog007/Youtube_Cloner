fcc07a7985fece32b5be69def4ec444a815160b0
海豹 <007seadog@gmail.com>
2017-05-07 05:50:06 +0000

    Added Usage Messages

--- del.sh
+++ del.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+[ -z "$1" ] && echo 'Usage: ./del.sh <error.log>' && exit 1
+
 while read line 
 do
 while read line; do gdrive delete -r $line; done
--- unique.sh
+++ unique.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+[ -z "$1" ] && echo 'Usage: ./unique.sh <file_you_want_to_unique>' && exit 1
+
 sort -u $1 > s
 mv s $1
