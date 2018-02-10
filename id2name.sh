b0feb1158489a0e8dca409b9df1a280d0a20426f
Jasper Yu <007seadog@gmail.com>
2018-02-10 18:45:14 +0800

    Removed unnecessary sed

--- id2name.sh
+++ id2name.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gdrive info `gdrive list -q "name contains '$1'" | grep '\-.\{11\}\.webm' | head -n1 | awk '{print $1}'` | sed -n 2p | grep -oP 'Name:\ \K.*(?=-.{11}\.webm)'
+gdrive info `gdrive list -q "name contains '$1'" | grep '\-.\{11\}\.webm' | head -n1 | awk '{print $1}'` | grep -oP 'Name:\ \K.*(?=-.{11}\.webm)'
