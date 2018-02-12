e1327b37bcae3c7500c830c7dd8ef7fb880b68cf
Jasper Yu <007seadog@gmail.com>
2018-02-13 03:29:52 +0800

    Updated og.php

--- src/og.php
+++ src/og.php
@@ -1,7 +1,11 @@
 <meta property="og:title" content="Kexor" />
 <meta property="og:type" content="website" />
+<meta property="og:url" content="https://seadog007.download" />
+<meta property="fb:app_id" content="<?= FB_API ?>" />
 <meta property="og:description" content="Searching for removed videos from Youtube?" />
-<meta property="og:image" content="http://seadog007.download/images/logo.jpg" />
+<meta property="og:image" content="https://seadog007.download/images/logo.png" />
+<meta property="og:image:height" content="512" />
+<meta property="og:image:width" content="512" />
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-113885780-1"></script>
 <script>
