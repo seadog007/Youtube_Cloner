05304d437c513f8e1b9aac27178aaf83edaf0daa
海豹 <007seadog@gmail.com>
2018-02-13 02:01:54 +0800

    Indent Improved

--- mkdir.sh
+++ mkdir.sh
@@ -4,14 +4,14 @@ shopt -s xpg_echo # 2% gain (against my expectations)
 set {a..z} {A..Z} {0..9} - _
 echo '{'
 for a do
-echo '"'$a'": {'
-av=`gdrive mkdir -p 0B7GYRmQ_BOeKam5tNWRWZ3lETlE $a | sed -e "s/Directory\ //" -e "s/\ created//"`
-for b do
-bv=`gdrive mkdir -p $av $b | sed -e "s/Directory\ //" -e "s/\ created//"`
-echo '"'$b'": "'$bv'",'
-[ "$b" == "_" ] && echo '".": "'$av'"'
-sleep 0.2
-done
-echo '},'
+	echo '"'$a'": {'
+	av=`gdrive mkdir -p 0B7GYRmQ_BOeKam5tNWRWZ3lETlE $a | sed -e "s/Directory\ //" -e "s/\ created//"`
+	for b do
+		bv=`gdrive mkdir -p $av $b | sed -e "s/Directory\ //" -e "s/\ created//"`
+		echo '"'$b'": "'$bv'",'
+		[ "$b" == "_" ] && echo '".": "'$av'"'
+		sleep 0.2
+	done
+	echo '},'
 done
 echo '}'
